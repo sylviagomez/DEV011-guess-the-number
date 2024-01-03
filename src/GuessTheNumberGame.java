34506069d9260aaba4743af5001910553b77a4cb
sylviagomez <syl.gomez.bio@gmail.com>
2024-01-03 12:13:30 -0600

    name+makeguess

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -4,7 +4,7 @@ public class GuessTheNumberGame {
     public static void main(String[] args) {
         // Crear un jugador humano
         Player player = new HumanPlayer("DefaultName");
-        // Llamar al método playerName() para que el jugador humano ingrese su nombre y realice alguna acción
+        // Llamar al método getName() para que el jugador humano ingrese su nombre y realice alguna acción
         player.getName();
         player.makeGuess();
     }
